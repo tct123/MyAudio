784e13dca569ab769139d4c1d7bd0713364294c2
tct123 <42028373+tct123@users.noreply.github.com>
2024-12-09 08:59:53 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.adaptive = True
+    audio1 = ft.Audio(
+        src="https://luan.xyz/files/audio/ambient_c_motion.mp3", autoplay=True
+    )
+    body = ft.Column(
+        controls=[
+            ft.Text("This is an app with background audio."),
+            ft.ElevatedButton("Stop playing", on_click=lambda _: audio1.pause()),
+        ]
+    )
+    page.overlay.append(audio1)
+    page.add(ft.SafeArea(body))
+
+
+ft.app(main)
